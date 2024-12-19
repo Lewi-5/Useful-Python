07e83359a1ffdff67e9a88072d513dc0bef1a258
Lewis <lewinnesmiller@gmail.com>
2024-12-19 17:00:48 -0500

    generator expressions!

--- iterators_etc/iterators.py
+++ iterators_etc/iterators.py
@@ -18,3 +18,11 @@ next(first_iter)
 
 print(f"first iter: {list(first_iter)[0]}")
 print(f"second iter: {list(second_iter)[0]}")
+
+# another interesting way of doing the check no elems above limit question
+# what is interesting here is that the first argument passed to the next
+# function is not a case of list comprehension, notice the parentheses
+# and no square brackets. This is creating a generator not a list! So this
+# is a case of a generator expression/generator comprehension:
+def small_enough(array, limit):
+    return next((False for i in array if i > limit), True)
